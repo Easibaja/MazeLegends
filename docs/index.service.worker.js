20c9eef360a62372c1ff55ab1059449bf7868831
Esibaja <106495696+Easibaja@users.noreply.github.com>
2026-06-01 15:11:00 -0600

    Update index.service.worker.js

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1780348115|1400155222';
+const CACHE_VERSION = '1780348237|1521882248';
 /** @type {string} */
 const CACHE_PREFIX = 'Maze-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
